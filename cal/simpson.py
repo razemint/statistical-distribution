3d9deaab33de62ee1a509dd475440cb0220dd617
jchristianmartinezg@gmail.com <jchristianmartinezg@gmail.com>
2019-06-02 19:39:21 -0500

    added simpson 1/3 and modules/packages

---
+++ cal/simpson.py
@@ -0,0 +1,29 @@
+from math import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def simpson13(n, a, b, f):
+    h = (b - a) / n
+
+    suma = 0.0
+
+    for i in range(1, n):
+        x = a + i * h
+
+        if(i % 2 == 0):
+            suma = suma + 2 * fx(x, f)
+        else:
+            suma = suma + 4 * fx(x, f)
+
+    suma = suma + fx(a, f) + fx(b, f)
+    rest = suma * (h / 3)
+
+    return (rest)
+
+
+def fx(x, f):
+    return eval(f)
+
+
+
---
+++ inicio.py
@@ -0,0 +1,10 @@
+from cal.simpson import simpson13, fx
+
+n = int(input("inserta el número de iteraciones\n"))
+a = float(input("inserta intervalo inferior\n"))
+b = float(input("inserta intervalo superior\n"))
+f = str(input("inserta la función\n"))
+
+
+print("el resultado de la integral es: ")
+print(simpson13(n, a, b, f))
