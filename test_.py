0aa0e75f31d6841770293e081e92d42a17975ea0
jchristianmartinezg@gmail.com <jchristianmartinezg@gmail.com>
2019-06-04 05:32:46 -0500

    some test

---
+++ test_.py
@@ -0,0 +1,13 @@
+print("haras una puta suma o una puta resta?")
+inputa = str(input())
+
+if inputa == ("suma"):
+    resultado = 1+1
+    print(resultado)
+
+elif inputa == ("resta"):
+    resultado = 1-1
+    print(resultado)
+
+else:
+    print("vales verga")
